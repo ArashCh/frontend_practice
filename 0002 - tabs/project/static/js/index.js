2e0b1bffea3bb3f877447f55ea71a594dc50b32c
ArashCh <a.arash.chitgar@gmail.com>
2022-12-27 02:29:29 +0330

    finished styling and started JS

---
+++ 0002 - tabs/project/static/js/index.js
@@ -0,0 +1,37 @@
+const DATA = {
+    "motorsports": {
+        "img_src": "../media/motorsports.jpg",
+        "content": "Motorsport, motorsports or motor sport is a global term \
+         used to encompass the group of competitive sporting events which \
+         primarily involve the use of motorized vehicles. The terminology can \
+         also be used to describe forms of competition of two-wheeled motorised \
+         vehicles under the banner of motorcycle racing, and includes off-road racing such as motocross.",
+    },
+    "fighter aircrafts": {
+        "img_src": "../media/fighter_aircraft.jpg",
+        "content": "Fighter aircraft are fixed-wing military aircraft \
+        designed primarily for air-to-air combat. In military conflict, the \
+        role of fighter aircraft is to establish air superiority of the battlespace. \
+        Domination of the airspace above a battlefield permits bombers and attack \
+        aircraft to engage in tactical and strategic bombing of enemy targets.",
+    },
+    "programming": {
+        "img_src": "../media/programming.jpg",
+        "content": "Computer programming is the process of performing a \
+        particular computation (or more generally, accomplishing a specific \
+            computing result), usually by designing and building an executable \
+            computer program. Programming involves tasks such as analysis, \
+            generating algorithms, profiling algorithms' accuracy and resource \
+            consumption, and the implementation of algorithms (usually in a chosen \
+            programming language, commonly referred to as coding).[1][2] \
+            The source code of a program is written in one or more languages \
+            that are intelligible to programmers, rather than machine code, \
+            which is directly executed by the central processing unit. The \
+            purpose of programming is to find a sequence of instructions that \
+            will automate the performance of a task (which can be as complex \
+            as an operating system) on a computer, often for solving a \
+            given problem. Proficient programming thus usually requires \
+            expertise in several different subjects, including knowledge \
+            of the application domain, specialized algorithms, and formal logic.",
+    }
+}
