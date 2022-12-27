754855f693b92db0bca8644ed9c8c2ebd7749ec8
ArashCh <a.arash.chitgar@gmail.com>
2022-12-27 03:55:59 +0330

    finished tabs project

--- 0002 - tabs/project/static/js/index.js
+++ 0002 - tabs/project/static/js/index.js
@@ -1,22 +1,25 @@
 const DATA = {
     "motorsports": {
-        "img_src": "../media/motorsports.jpg",
+        "title": "Motorsports",
+        "img_src": "./static/media/motorsports.jpg",
         "content": "Motorsport, motorsports or motor sport is a global term \
          used to encompass the group of competitive sporting events which \
          primarily involve the use of motorized vehicles. The terminology can \
          also be used to describe forms of competition of two-wheeled motorised \
-         vehicles under the banner of motorcycle racing, and includes off-road racing such as motocross.",
+         vehicles under the banner of motorcycle racing, and includes off-road racing such as motocross."
     },
-    "fighter aircrafts": {
-        "img_src": "../media/fighter_aircraft.jpg",
+    "fighter": {
+        "title": "Fighter Aircraft",
+        "img_src": "./static/media/fighter_aircraft.jpg",
         "content": "Fighter aircraft are fixed-wing military aircraft \
         designed primarily for air-to-air combat. In military conflict, the \
         role of fighter aircraft is to establish air superiority of the battlespace. \
         Domination of the airspace above a battlefield permits bombers and attack \
-        aircraft to engage in tactical and strategic bombing of enemy targets.",
+        aircraft to engage in tactical and strategic bombing of enemy targets."
     },
     "programming": {
-        "img_src": "../media/programming.jpg",
+        "title": "Programming",
+        "img_src": "./static/media/programming.jpg",
         "content": "Computer programming is the process of performing a \
         particular computation (or more generally, accomplishing a specific \
             computing result), usually by designing and building an executable \
@@ -30,8 +33,32 @@ const DATA = {
             purpose of programming is to find a sequence of instructions that \
             will automate the performance of a task (which can be as complex \
             as an operating system) on a computer, often for solving a \
-            given problem. Proficient programming thus usually requires \
-            expertise in several different subjects, including knowledge \
-            of the application domain, specialized algorithms, and formal logic.",
+            given problem."
     }
+}
+$(".tab").click(event => {
+    let tabId = "";
+    switch (event.target.id) {
+        case "tab_motorsports":
+            tabId = "tab_motorsports";
+            break;
+        case "tab_fighter":
+            tabId = "tab_fighter";
+            break;
+        case "tab_programming":
+            tabId = "tab_programming";
+            break;
+        default:
+            break;
+    }
+    $("#desc_title").text(DATA[tabId.slice(4)]["title"]);
+    $("#desc_content").text(DATA[tabId.slice(4)]["content"])
+    $("#img").attr("src", DATA[tabId.slice(4)]["img_src"]);
+    setTabsStyle(tabId);
+});
+function setTabsStyle(tabId) {
+    $(".tab").css("background-color", "#d9e3ed");
+    $(".tab").css("border-color", "#d9e3ed");
+    $("#"+tabId).css("background-color", "#ffffff");
+    $("#"+tabId).css("border", "solid 2px #4e6d9c");
 }
