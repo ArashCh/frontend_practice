81696f5c3424a66ecff03ad31a5b3b57cbe95167
ArashCh <a.arash.chitgar@gmail.com>
2023-01-06 00:42:11 +0330

    0004 - the cocktail db, finished home page except responsivity

--- 0004 - the cocktail db/project/static/js/index.js
+++ 0004 - the cocktail db/project/static/js/index.js
@@ -1,39 +1,96 @@
-var DATA = {};
+loadingBars(true);
 
-$.getJSON("/0004 - the cocktail db/project/cocktail_data.json", (json) => {
-      // getting the data
-      DATA = json;
+var cocktail_data;
 
-      let cocktails = $("#cocktails");
-      $(cocktails).hide();
-      (function () {
-            for (let k in DATA) {
-                  let cocktail = $("<div>");
-                  $(cocktail).addClass("cocktail");
-                  let picture = $("<picture>");
-                  let img = $("<img>");
-                  $(img).attr("src", DATA[k]["img_src"]);
-                  $(img).attr("alt", DATA[k]["name"]);
-                  let content = $("<div>");
-                  $(content).addClass("content");
-                  let h3 = $("<h3>");
-                  $(h3).text(DATA[k]["name"]);
-                  let h4 = $("<h4>");
-                  $(h4).text(DATA[k]["glass"]);
-                  let p = $("<p>");
-                  $(p).text(DATA[k]["info"]);
-                  let a = $("<a>");
-                  $(a).text("DETAILS");
-            
-                  $(content).append(h3);
-                  $(content).append(h4);
-                  $(content).append(p);
-                  $(content).append(a);
-                  $(picture).append(img);
-                  $(cocktail).append(picture);
-                  $(cocktail).append(content);
-                  $(cocktails).append(cocktail);
+loadData()
+
+function loadData(search="") {
+      let url = search.length < 1 ? "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=": "https://www.thecocktaildb.com/api/json/v1/1/search.php?s="+search
+      let drinks
+      $.getJSON(url, null , (j) =>{
+            drinks = j.drinks;
+            if (drinks) {
+                  showDrinks(drinks);
+            } else {
+                  noMatches();
             }
-      })();
-      $(cocktails).show();
+      });
+}
+
+function showDrinks(drinks) {
+      let cocktails = $("#cocktails");
+      // clear current content
+      $(cocktails).empty();
+      loadingBars(true);
+
+      for (let k of Object.keys(drinks)) {
+            let drink = drinks[k]
+            // get data
+            let img_src = drink["strDrinkThumb"];
+            let name = drink["strDrink"];
+            let info = drink["strAlcoholic"];
+            let glass = drink["strGlass"];
+            let ingredients = []
+            for (let i=1 ; i<=15 ; i++) {
+                  let ing = drink["strIngredient"+i];
+                  if (ing != null) {
+                        ingredients.push(ing);
+                  } else {
+                        break;
+                  }
+            }
+            // create and fill the DOMs
+            // cocktail card
+            let cocktail = $("<div>");
+            $(cocktail).addClass("cocktail");
+            // cocktail picture
+            let picture = $("<picture>");
+            let img = $("<img>");
+            $(img).attr("src", img_src);
+            $(img).attr("alt", name);
+            // content part
+            let content = $("<div>");
+            $(content).addClass("content");
+            let h2 = $("<h2>");
+            $(h2).text(name);
+            let h3 = $("<h3>");
+            $(h3).text(glass[0].toUpperCase() + glass.slice(1));
+            let p = $("<p>");
+            $(p).text(info);
+            let a = $("<a>");
+            $(a).text("DETAILS");
+            // adding elements to the DOM
+            $(content).append(h2);
+            $(content).append(h3);
+            $(content).append(p);
+            $(content).append(a);
+            $(picture).append(img);
+            $(cocktail).append(picture);
+            $(cocktail).append(content);
+            $(cocktails).append(cocktail);
+      }
+      loadingBars(false);
+}
+
+$("#search").keyup((event) => {
+      loadData(search=event.target.value);
 });
+
+function loadingBars(show=true) {
+      $("#noData").hide();
+      if (show) {
+            $("#loadingBars").show()
+            $("h1").hide();
+            $("#cocktails").hide();
+      } else {
+            $("#loadingBars").hide()
+            $("h1").show();
+            $("#cocktails").show();
+      }
+}
+
+function noMatches() {
+      $("#cocktails").hide();
+      $("#noData").show();
+      $("h1").hide();
+}
