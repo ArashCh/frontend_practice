beead7d37f07d566cff3b75913f6ddb8b2641290
ArashCh <a.arash.chitgar@gmail.com>
2023-01-18 02:09:47 +0330

    0011 - ping pong, finished JS

--- 0011 - ping pong/project/static/js/index.js
+++ 0011 - ping pong/project/static/js/index.js
@@ -8,7 +8,7 @@ const ballAttrs = {
     "posX":board.offsetWidth/2,
     "posY":board.offsetHeight/2,
     "speedX": 2,
-    "speedY": 2
+    "speedY": 2,
 }
 const paddleLeftAttrs = {
     "posY":board.offsetHeight/2 - paddleHeight/2
@@ -17,7 +17,10 @@ const paddleRightAttrs = {
     "posY":board.offsetHeight/2 - paddleHeight/2,
     "speedY": 2
 }
-
+const scores = {
+    "player": 0,
+    "CPU": 0
+}
 
 ball.style.left = ballAttrs.posX + "px";
 ball.style.top = ballAttrs.posY + "px";
@@ -28,6 +31,9 @@ paddleLeft.style.top = paddleLeftAttrs.posY + "px";
 paddleRight.style.right = "0px";
 paddleRight.style.top = paddleRightAttrs.posY + "px";
 
+let updateScoreBoard = () => {
+    document.getElementById("scoreBoard").textContent = "You "+scores.player+" - CPU "+scores.CPU;
+};
 
 let CPUMove = () => {
     const paddleRightEdgeTop = paddleRight.offsetTop;
@@ -36,19 +42,33 @@ let CPUMove = () => {
     if (paddleRightEdgeTop < 0) paddleRightAttrs.speedY = -paddleRightAttrs.speedY;
     if (paddleRightEdgeBottom+10 > board.offsetHeight) paddleRightAttrs.speedY = -paddleRightAttrs.speedY;
 
-    paddleRightAttrs.posY += paddleRightAttrs.speedY;
+    if (ballAttrs.posY > (paddleRightAttrs.posY + (paddleHeight/2))) {
+        paddleRightAttrs.posY += paddleRightAttrs.speedY;
+    } else {
+        paddleRightAttrs.posY -= paddleRightAttrs.speedY;
+    }
     paddleRight.style.top =  paddleRightAttrs.posY  + "px";
 };
 
 let ballPhysics = (ball) => {
+    // if the ball hits the vertical edges
     const ballEdgeRight = ballAttrs.posX + ball.offsetWidth;
-    if ((ballEdgeRight > board.offsetWidth) || (ballAttrs.posX < 0)) {
+    if (ballEdgeRight > board.offsetWidth) {
         ballAttrs.speedX = -ballAttrs.speedX;
+        scores.player += 1;
+        updateScoreBoard();
     }
+    if (ballAttrs.posX < 0) {
+        ballAttrs.speedX = -ballAttrs.speedX;
+        scores.CPU += 1;
+        updateScoreBoard();
+    }
+    // if the ball hits the horizontal edges
     const ballEdgeBottom = ballAttrs.posY + ball.offsetHeight;
     if ((ballEdgeBottom > board.offsetHeight) || (ballAttrs.posY < 0)) {
         ballAttrs.speedY = -ballAttrs.speedY;
     }
+
     ballAttrs.posX += ballAttrs.speedX;
     ball.style.left = ballAttrs.posX + "px";
     ballAttrs.posY += ballAttrs.speedY;
@@ -57,67 +77,47 @@ let ballPhysics = (ball) => {
 
 let CheckPaddleCollision = () => {
     const paddleLeftEdgeTop = paddleLeft.offsetTop;
-    const paddleLeftEdgeRight = paddleLeft.offsetLeft + paddleWidth;
+    const paddleLeftEdgeRight = paddleWidth;
     const paddleLeftEdgeBottom = paddleLeftEdgeTop + paddleHeight;
     
     const paddleRightEdgeTop = paddleRight.offsetTop;
     const paddleRightEdgeBottom = paddleRightEdgeTop + paddleHeight;
     const paddleRightEdgeLeft = paddleRight.offsetLeft;
 
-    const ballEdgeTop = ballAttrs.posY;
-    const ballEdgeBottom = ballAttrs.posY + ball.offsetHeight;
-    const ballEdgeRight = ballAttrs.posX + ball.offsetWidth;
-    const ballEdgeLeft = ballAttrs.posX;
+    const ballMargin = 5;
+
+    const ballEdgeTop = ballAttrs.posY - ballMargin;
+    const ballEdgeBottom = ballAttrs.posY + ball.offsetHeight + ballMargin;
+    const ballEdgeRight = ballAttrs.posX + ball.offsetWidth + ballMargin;
+    const ballEdgeLeft = ballAttrs.posX - ballMargin;
 
     if (
             (
                 (ballEdgeLeft <= paddleLeftEdgeRight) &&
                 (ballEdgeBottom >= paddleLeftEdgeTop) &&
-                (ballEdgeTop <= paddleLeftEdgeBottom) &&
-                (ballEdgeLeft >= 0)
+                (ballEdgeTop <= paddleLeftEdgeBottom)
             )
             ||
             (
-                (ballEdgeRight <= paddleRightEdgeLeft) &&
-                (ballEdgeBottom > paddleRightEdgeTop) &&
-                (ballEdgeTop < paddleRightEdgeBottom) &&
-                (ballEdgeRight > board.offsetWidth)
-            )
-        ) {
-            ballAttrs.speedX = -ballAttrs.speedX;
-    }
-    if (
-            (
-                (ballEdgeLeft <= paddleLeftEdgeRight) &&
-                (ballEdgeBottom > paddleLeftEdgeTop) &&
-                (ballEdgeTop < paddleLeftEdgeBottom) &&
-                (ballEdgeLeft > 0)
-            )
-            ||
-            (
-                (ballEdgeRight <= paddleRightEdgeLeft) &&
-                (ballEdgeBottom > paddleRightEdgeTop) &&
-                (ballEdgeTop < paddleRightEdgeBottom) &&
-                (ballEdgeRight > board.offsetWidth)
+                (ballEdgeRight >= paddleRightEdgeLeft) &&
+                (ballEdgeBottom >= paddleRightEdgeTop) &&
+                (ballEdgeTop <= paddleRightEdgeBottom)
             )
         ) {
             ballAttrs.speedX = -ballAttrs.speedX;
     }
 };
 
-
 setInterval(()=>{
     ballPhysics(ball);
     CPUMove();
     CheckPaddleCollision();
-}, 80);
+}, 10);
 
 board.addEventListener("mousemove", (event)=>{
     const boardEdgeTop = board.offsetTop - (board.offsetHeight/2)
     const mouseY = event.clientY - boardEdgeTop;
-    console.log(mouseY);
     if (mouseY < 0) return
     if ((mouseY+paddleHeight+10) > board.offsetHeight) return
     paddleLeft.style.top = mouseY + "px";
 });
-
