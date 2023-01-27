d792812f1525cacd7ff6d69e82eef24c116fbfb0
ArashCh <a.arash.chitgar@gmail.com>
2023-01-27 16:39:31 +0330

    finished analog clock

--- 0008 - clock/project/static/js/index.js
+++ 0008 - clock/project/static/js/index.js
@@ -6,7 +6,12 @@
     const second = time.getSeconds() < 10 ? "0"+time.getSeconds():time.getSeconds();
     const ampm = hour < 12? "am":"pm";
     
+    // for digital clock
     $(".clock_digital").text(hour+":"+minute+":"+second+" "+ampm);
+    // for analog clock
+    $("#analogHour").css("transform", "rotate("+(parseInt(hour)-12)*30+"deg)");
+    $("#analogMin").css("transform", "rotate("+parseInt(minute)*6+"deg)");
+    $("#analogSec").css("transform", "rotate("+parseInt(second)*6+"deg)");
 })();
 
 
@@ -18,5 +23,10 @@ const interval = setInterval(()=>{
     const second = time.getSeconds() < 10 ? "0"+time.getSeconds():time.getSeconds();
     const ampm = hour < 12? "am":"pm";
     
+    // for digital clock
     $(".clock_digital").text(hour+":"+minute+":"+second+" "+ampm);
+    // for analog clock
+    $("#analogHour").css("transform", "rotate("+(parseInt(hour)-12)*30+"deg)");
+    $("#analogMin").css("transform", "rotate("+parseInt(minute)*6+"deg)");
+    $("#analogSec").css("transform", "rotate("+parseInt(second)*6+"deg)");
 }, 1000);
