91a28d6cd0ccdf542e09c7da9585894b3ab0f346
ArashCh <a.arash.chitgar@gmail.com>
2023-01-27 16:43:41 +0330

    improved code and added analog clock

--- 0008 - clock/project/static/js/index.js
+++ 0008 - clock/project/static/js/index.js
@@ -1,4 +1,4 @@
-(()=>{
+const setTime = ()=> {
     const time = new Date();
 
     const hour = time.getHours() < 10 ? "0"+time.getHours():time.getHours();
@@ -12,21 +12,12 @@
     $("#analogHour").css("transform", "rotate("+(parseInt(hour)-12)*30+"deg)");
     $("#analogMin").css("transform", "rotate("+parseInt(minute)*6+"deg)");
     $("#analogSec").css("transform", "rotate("+parseInt(second)*6+"deg)");
+};
+
+(()=>{
+    setTime();
 })();
 
-
 const interval = setInterval(()=>{
-    const time = new Date();
-
-    const hour = time.getHours() < 10 ? "0"+time.getHours():time.getHours();
-    const minute = time.getMinutes() < 10 ? "0"+time.getMinutes():time.getMinutes();
-    const second = time.getSeconds() < 10 ? "0"+time.getSeconds():time.getSeconds();
-    const ampm = hour < 12? "am":"pm";
-    
-    // for digital clock
-    $(".clock_digital").text(hour+":"+minute+":"+second+" "+ampm);
-    // for analog clock
-    $("#analogHour").css("transform", "rotate("+(parseInt(hour)-12)*30+"deg)");
-    $("#analogMin").css("transform", "rotate("+parseInt(minute)*6+"deg)");
-    $("#analogSec").css("transform", "rotate("+parseInt(second)*6+"deg)");
+    setTime();
 }, 1000);
