3f676ff0508d3b8d3cf7549d75ffcad8e2701757
ArashCh <a.arash.chitgar@gmail.com>
2023-01-27 13:42:03 +0330

    improved digital clock, working on analog

--- 0008 - clock/project/static/js/index.js
+++ 0008 - clock/project/static/js/index.js
@@ -1,10 +1,22 @@
-const interval = setInterval(()=>{
-    let time = new Date();
+(()=>{
+    const time = new Date();
 
-    let hour = time.getHours() < 10 ? "0"+time.getHours():time.getHours();
-    let minute = time.getMinutes() < 10 ? "0"+time.getMinutes():time.getMinutes();
-    let second = time.getSeconds() < 10 ? "0"+time.getSeconds():time.getSeconds();
-    let ampm = hour < 12? "am":"pm";
+    const hour = time.getHours() < 10 ? "0"+time.getHours():time.getHours();
+    const minute = time.getMinutes() < 10 ? "0"+time.getMinutes():time.getMinutes();
+    const second = time.getSeconds() < 10 ? "0"+time.getSeconds():time.getSeconds();
+    const ampm = hour < 12? "am":"pm";
     
-    $("#clock").text(hour+":"+minute+":"+second+" "+ampm);
+    $(".clock_digital").text(hour+":"+minute+":"+second+" "+ampm);
+})();
+
+
+const interval = setInterval(()=>{
+    const time = new Date();
+
+    const hour = time.getHours() < 10 ? "0"+time.getHours():time.getHours();
+    const minute = time.getMinutes() < 10 ? "0"+time.getMinutes():time.getMinutes();
+    const second = time.getSeconds() < 10 ? "0"+time.getSeconds():time.getSeconds();
+    const ampm = hour < 12? "am":"pm";
+    
+    $(".clock_digital").text(hour+":"+minute+":"+second+" "+ampm);
 }, 1000);
